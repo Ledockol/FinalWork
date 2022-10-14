0e83eb2492f46baf0682ee73a29903959fe0b190
led Ice <Andrey-ssk98@yandex.ru>
2022-10-14 22:27:03 +0300

    finalCod

---
+++ Work/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите количество чисел");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[size];
+FillArrayRandomNumbers(array);
+PrintArray(array);
+void FillArrayRandomNumbers(double[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        {
+            array[i] = Convert.ToDouble(new Random().Next(0,10000));
+        }
+}
+void PrintArray(double[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        {
+            Console.Write($"{array[i]} ");
+        }
+    Console.WriteLine();
+}
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[i] <= 999)
+    Console.Write(array[i] + " " );
+}
